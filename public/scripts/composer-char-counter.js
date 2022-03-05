cb1498edbba614f4f6faaaa9f136b88b8c40c442
Francis <fwang36@gmail.com>
2022-03-05 15:05:26 -0500

    added counter change based on pasting

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,14 +2,22 @@
 $(() => {
   $("#tweet-text").keypress(function(e) {
     let counter = 140 - $("#tweet-text").val().length - 1;
-
     e.target.form['2'].innerHTML = counter;
     if (e.target.form['2'].innerHTML < 0) {
       $(e.target.form['2']).css('color', 'red');
     }
     if (e.target.form['2'].innerHTML > 0) {
       $(e.target.form['2']).css('color', "#545149");
-
     }
   });
+
+
+//Changes counter if input is pasted into
+  $("#tweet-text").on("paste",function(e) {
+   setTimeout(function() {
+    let counter = 140
+    console.log($("#tweet-text").val().length)
+    e.target.form['2'].innerHTML = counter - $("#tweet-text").val().length
+   }, 100)
+  })
 });
